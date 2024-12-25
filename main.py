009920dbf368f5c51aa9d16ab5c0ec396fcf41f9
Benjy <108727379+Benjy0812@users.noreply.github.com>
2024-12-25 21:36:39 +0100

    added quit
    Made so you can quit the script by typing quit upon entering the first number

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-print("This is a calculator")
+while True:    
+    print("This is a calculator\nEnter quit to quit")
 
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+    num1 = input("Enter the first number: ")
+    
+    if num1 == 'quit':
+      break
+  
+    num2 = input("Enter the second number: ")
+    
 
-operation = input("chose opertation:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n")
+    operation = input("chose opertation:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n")
+    
+    num_1 = float(num1)
+    num_2 = float(num2)
 
-if operation == '1':
-    print(f"The result is: {num1 + num2}")
-elif operation == '2':
-    print(f"The result is: {num1 - num2}")
-elif operation == '3':
-    print(f"Ther result is: {num1 * num2}")
-elif operation == '4':
-    if num2 != 0:
-        print(f"The result is {num1 / num2}")
+    if operation == '1':
+        print(f"The result is: {num_1 + num_2}")
+    elif operation == '2':
+        print(f"The result is: {num_1 - num_2}")
+    elif operation == '3':
+        print(f"Ther result is: {num_1 * num_2}")
+    elif operation == '4':
+        if num2 != 0:
+            print(f"The result is {num_1 / num_2}")
+        else:
+            print("Error: Division bt zero i not allowed")
+
     else:
-        print("Error: Division bt zero i not allowed")
-
-else:
-    print("Invalid choice! pleasse run the program agein and choose a valid option")
-
-
+        print("Invalid choice! pleasse run the program agein and choose a valid option")
