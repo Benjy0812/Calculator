22b6581c55c0ce391f2cfc987e47af98cdb477b3
Benjy <108727379+Benjy0812@users.noreply.github.com>
2024-12-26 00:11:59 +0100

    Update

--- main.py
+++ main.py
@@ -1,30 +1,57 @@
-while True:    
-    print("This is a calculator\nEnter quit to quit")
+import os
 
-    num1 = input("Enter the first number: ")
-    
-    if num1 == 'quit':
-      break
-  
-    num2 = input("Enter the second number: ")
-    
 
-    operation = input("chose opertation:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\nEnter operation: ")
-    
-    num_1 = float(num1)
-    num_2 = float(num2)
-
-    if operation == '1':
-        print(f"The result is: {num_1 + num_2}")
-    elif operation == '2':
-        print(f"The result is: {num_1 - num_2}")
-    elif operation == '3':
-        print(f"Ther result is: {num_1 * num_2}")
-    elif operation == '4':
-        if num2 != 0:
-            print(f"The result is {num_1 / num_2}")
-        else:
-            print("Error: Division bt zero i not allowed")
-
+def clear(): 
+    if os.name == 'nt':
+        os.system('cls')  # Windows
     else:
-        print("Invalid choice! pleasse run the program agein and choose a valid option")
+        os.system('clear')  # macOS/Linux
+
+def caclulator():
+    while True:
+        print()
+          
+        print("This is a calculator\nEnter 'quit' to quit")
+
+        print()
+
+        num1 = input("Enter the first number: ")
+        
+        if num1.lower() == 'quit':
+            clear()  
+            break
+    
+        num2 = input("Enter the second number: ")
+        
+        print()
+
+        operation = input("chose opertation:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\nEnter operation: ")
+        
+        clear()
+        
+        try:
+            num_1 = float(num1)
+            num_2 = float(num2)
+        except ValueError:
+            print("Error: please enter valid numbers")
+            continue
+
+        print()
+
+        if operation == '1':
+            print(f"The result is: {num_1 + num_2}")
+        elif operation == '2':
+            print(f"The result is: {num_1 - num_2}")
+        elif operation == '3':
+            print(f"Ther result is: {num_1 * num_2}")
+        elif operation == '4':
+            if num2 != 0:
+                print(f"The result is {num_1 / num_2}")
+            else:
+                print("Error: Division by zero i not allowed")
+        else:
+            print("Invalid choice! pleasse run the program again and choose a valid option")
+            
+        print()
+
+caclulator()
