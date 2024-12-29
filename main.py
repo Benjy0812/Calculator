a8b27e129ef9b741881577d4d8a228ea5a5fdc23
Benjy <108727379+Benjy0812@users.noreply.github.com>
2024-12-29 21:44:05 +0100

    linting👍

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import os
 
 
-def clear(): 
+def clear():
     if os.name == 'nt':
         os.system('cls')  # Windows
     else:
         os.system('clear')  # macOS/Linux
 
-def caclulator():
+
+def calculator():
+
     while True:
         print()
-          
+
         print("This is a calculator\nEnter 'quit' to quit")
 
         print()
 
         num1 = input("Enter the first number: ")
         if num1.lower() == 'quit':
-            clear()  
+            clear()
             break
-    
+
         num2 = input("Enter the second number: ")
-        
+
         print()
 
-        operation = input("chose opertation:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\nEnter operation: ")
-        
+        operation = input("chose operation:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\nEnter operation: ")
+
         clear()
-        
+
         try:
             num_1 = float(num1)
             num_2 = float(num2)
@@ -42,15 +44,16 @@ def caclulator():
         elif operation == '2':
             print(f"The result is: {num_1 - num_2}")
         elif operation == '3':
-            print(f"Ther result is: {num_1 * num_2}")
+            print(f"There result is: {num_1 * num_2}")
         elif operation == '4':
             if num2 != 0:
                 print(f"The result is {num_1 / num_2}")
             else:
                 print("Error: Division by zero i not allowed")
         else:
-            print("Invalid choice! pleasse run the program again and choose a valid option")
-            
+            print("Invalid choice! please run the program again and choose a valid option")
+
         print()
 
-caclulator()
+
+calculator()
