fcf6c0d4491759d78640ecbe11da6eab2d64b787
Benjy <108727379+Benjy0812@users.noreply.github.com>
2024-12-29 22:20:28 +0100

    Fixed main.py merge conflict

--- main.py
+++ main.py
@@ -10,33 +10,14 @@ def clear():
 
 
 def calculator():
-<<<<<<< HEAD
-
-    while True:
-        print()
-
-        print("This is a calculator\nEnter 'quit' to quit")
-=======
     while True:
         print()
 
         print("This is a calculator\nEnter 'exit' to exit")
->>>>>>> 650c9f10a00c0fbe03fad65f3b91295d026d4aa6
 
         print()
 
         num1 = input("Enter the first number: ")
-<<<<<<< HEAD
-        if num1.lower() == 'quit':
-            clear()
-            break
-
-        num2 = input("Enter the second number: ")
-
-        print()
-
-        operation = input("chose operation:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\nEnter operation: ")
-=======
         if num1.lower() == "exit":
             break
 
@@ -49,7 +30,6 @@ def calculator():
         print("3. Multiply")
         print("4. Divide")
         operation = input("Enter operation: ")
->>>>>>> 650c9f10a00c0fbe03fad65f3b91295d026d4aa6
 
         clear()
 
@@ -66,27 +46,17 @@ def calculator():
             print(f"The result is: {num_1 + num_2}")
         elif operation == "2":
             print(f"The result is: {num_1 - num_2}")
-<<<<<<< HEAD
-        elif operation == '3':
-            print(f"There result is: {num_1 * num_2}")
-        elif operation == '4':
-=======
         elif operation == "3":
             print(f"The result is: {num_1 * num_2}")
         elif operation == "4":
->>>>>>> 650c9f10a00c0fbe03fad65f3b91295d026d4aa6
             if num2 != 0:
                 print(f"The result is {num_1 / num_2}")
             else:
                 print("Error: Division by zero i not allowed")
         else:
-<<<<<<< HEAD
-            print("Invalid choice! please run the program again and choose a valid option")
-=======
             print(
                 "Invalid choice! please try again"
             )
->>>>>>> 650c9f10a00c0fbe03fad65f3b91295d026d4aa6
 
         print()
 
