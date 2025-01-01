805b733e90d7910b95fdd994d39e3a5b344eaac0
Benjy0812 <108727379+Benjy0812@users.noreply.github.com>
2025-01-01 02:56:28 +0100

    added exit at the end

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
+import sys
 
 
-def clear():
-
+def clear_console():
     if os.name == "nt":
         os.system("cls")
     else:
@@ -11,36 +11,27 @@ def clear():
 
 def calculator():
     while True:
-        print()
-
-        print("This is a calculator\nEnter 'exit' to exit")
-
-        print()
+        print("\nThis is a calculator")
 
         num1 = input("Enter the first number: ")
-        if num1.lower() == "exit":
-            break
 
         num2 = input("Enter the second number: ")
 
-        print()
-        print("chose operation:")
+        print("\nChoose an operation:")
         print("1. Add")
         print("2. Subtract")
         print("3. Multiply")
         print("4. Divide")
         operation = input("Enter operation: ")
 
-        clear()
+        clear_console()
 
         try:
             num_1 = float(num1)
             num_2 = float(num2)
         except ValueError:
-            print("Error: please enter valid numbers")
+            print("Error: Please enter valid numbers.")
             continue
-
-        print()
 
         if operation == "1":
             print(f"The result is: {num_1 + num_2}")
@@ -49,16 +40,16 @@ def calculator():
         elif operation == "3":
             print(f"The result is: {num_1 * num_2}")
         elif operation == "4":
-            if num2 != 0:
-                print(f"The result is {num_1 / num_2}")
+            if num_2 != 0:
+                print(f"The result is: {num_1 / num_2}")
             else:
-                print("Error: Division by zero i not allowed")
+                print("Error: Division by zero is not allowed.")
         else:
-            print(
-                "Invalid choice! please try again"
-            )
+            print("Invalid choice! Please try again.\n")
 
-        print()
+        exit = input("\nDo you want to perform another calculation? (yes/no)")
+        if exit.lower() == 'no':
+            sys.exit()
 
 
 calculator()
